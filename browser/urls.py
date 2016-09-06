d7f0c8d01009bce3ffc7155f3eb38933fab68172
Denis Kolokol <dkolokol@gmail.com>
2016-09-06 12:38:39 +0200

    pages video and about

--- browser/urls.py
+++ browser/urls.py
@@ -4,9 +4,19 @@ from . import views
 
 
 urlpatterns = (
-    url(r'^$', views.build_index, {
+    url(r'^$', views.view_index, {
         'label': 'index',
-        'template': 'collectionv',
-        'img': 'bg_main1.jpg'
+        'template': 'column_left',
+        'img': 'bg1.jpg'
+        }),
+    url(r'^watch/$', views.view_watch, {
+        'label': 'watch',
+        'template': 'row_top',
+        'img': 'bg5.jpg'
+        }),
+    url(r'^read/$', views.view_read, {
+        'label': 'about',
+        'template': 'row_bottom',
+        'img': 'bg4.jpg'
         }),
 )
--- browser/views.py
+++ browser/views.py
@@ -8,51 +8,66 @@ from django.shortcuts import render_to_response
 from django.conf import settings
 
 
-comp_name = 'moon2'
-
-
-def build_index(request, **kwargs):
-    """
-    Build Index page
-    """
-    # WARNING! Change to processing of the index when its ready
+def get_struct(request, content, **kwargs):
     page_label = kwargs.get('label', '')
     page_template = kwargs.get('template', '')
-    page_img = kwargs.get('img', 'bg_main1.jpg')
+    page_img = kwargs.get('img', 'bg1.jpg')
     if not page_template:
         raise Http404
-
     page_template = '.'.join([page_template, 'html'])
-    data_dict = get_data_index()
-    data = ((
-        data_dict['main'], data_dict['video'], data_dict['audio'],
-        ), )
     return render_to_response(page_template, {
         'page_title': get_page_title(page_label.strip()),
-        'data': data,
+        'content': content,
         'menu': get_menu(),
         'page_img': page_img,
         'static_url': settings.STATIC_URL
         })
 
 
-def get_page_title(lb):
-    """
-    Fill page title
-    """
-    page_ttl = comp_name
-    if lb:
-        page_ttl += ': ' +  lb
+def view_index(request, **kwargs):
+    """Builds Index page."""
+    cont = get_index()
+    content = (
+        (cont['main'], cont['video'], cont['audio'],),
+        )
+    return get_struct(request, content, **kwargs)
 
-    return page_ttl
+
+def view_watch(request, **kwargs):
+    """Builds 'watch' page (video)."""
+    cont = get_watch()
+    content = (
+        (cont['main'], ),
+        (cont['aux'], ),
+        (cont['media'],),
+        )
+    return get_struct(request, content, **kwargs)
+
+
+def view_read(request, **kwargs):
+    """Builds 'read' page (about)."""
+    cont = get_about()
+    content = (
+        (cont['main'], ),
+        (cont['media'],),
+        (cont['aux'], ),
+        )
+    return get_struct(request, content, **kwargs)
+
+
+def get_page_title(lb):
+    """Fills page title."""
+    if lb:
+        return "%s: %s" % (settings.PROJECT_NAME, lb)
+    return settings.PROJECT_NAME
 
 
 def get_menu():
     return [
-        {'label': 'index', 'uri': ''},
+        {'label': 'index', 'uri': '/'},
+        {'label': 'watch', 'uri': '/watch/'},
+        {'label': 'read', 'uri': '/read/'},
         {'label': 'listen', 'uri': 'http://sympliromatiko.net/audio/'},
-        {'label': 'watch', 'uri': 'http://sympliromatiko.net/video/'},
-        {'label': 'read', 'uri': 'http://sympliromatiko.net/about/'},
         {'label': 'rider', 'uri': 'http://sympliromatiko.net/tech-rider/'},
         {'label': 'contact', 'uri': 'http://sympliromatiko.net/contacts/'},
         ]
@@ -62,23 +77,72 @@ def get_menu():
 # extracting data
 # WARNING! to be extracted from db!!
 
-def get_data_index():
+def get_index():
     return {
-    'main': {
-        'title': 'MOON2',
-        'text': """<i>inhuman music made by humans for a humankind</i>
+    "main": {
+        "title": "MOON2",
+        "text": """<i>inhuman music made by humans for a humankind</i>
         <p>
         Imagine if you could touch the sounds surrounding you. Imagine if you could build a piece of music by simply grabbing and moving sounds around, as if they were sticks and stones. Imagine yourself becoming a Titan in a vast sonic Universe moving and smashing massive masses of sounds around you.
         <p>
         MOON2 (formely Sympli Romatikó) is a trio with more or less stable residence in Kraków, Poland. The group’s main focus is interactive performance, algorithmic composition, virtual worlds, this kind of stuff… Pretty scientific and inhuman, but made by humans and for a humankind.
         """
         },
-    'video': {
-        'title': '',
-        'text': """<iframe src="https://player.vimeo.com/video/134827501?title=0&byline=0&portrait=0" width="300" height="169" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>"""
+    "video": {
+        "title": "",
+        "text": """<iframe src="https://player.vimeo.com/video/134827501?title=0&byline=0&portrait=0" width="300" height="169" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>"""
         },
-    'audio': {
-        'title': '',
-        'text': """<iframe style="border: 0; width: 300px; height: 300px;" src="https://bandcamp.com/EmbeddedPlayer/album=858428838/size=large/bgcol=333333/linkcol=0f91ff/minimal=true/transparent=true/" seamless><a href="http://mathka.bandcamp.com/album/sympli-romatik">Sympli Romatikó by Sympli Romatikó</a></iframe>"""
+    "audio": {
+        "title": "",
+        "text": """<iframe style="border: 0; width: 300px; height: 300px;" src="https://bandcamp.com/EmbeddedPlayer/album=858428838/size=large/bgcol=333333/linkcol=0f91ff/minimal=true/transparent=true/" seamless><a href="http://mathka.bandcamp.com/album/sympli-romatik">Sympli Romatikó by Sympli Romatikó</a></iframe>"""
+    }
+}
+
+
+def get_watch():
+    return {
+    "main": {
+        "title": "MOON2",
+        "text": """<i>is a virtual world with simplified physical laws,</i>
+        <br>
+        where sounds turn into objects, whose behavior is affected by natural forces such as gravity and wind. Performer’s hands attract floating sounds – he literally “grasps” them and “draws” their trajectories in space. The space itself is a giant ephemeral musical instrument, which you neither pluck, nor bow, but excite through movement."""
+        },
+    "aux": {
+        "title": "",
+        "text": """It is neither about dance, nor interactive electronic music, nor whatever fancy label one can come up with. It is about a human body simultaneously operating in so-call reality as well as in the virtual World, which we can only hear.
+        <p>
+        Interested in having us around? Please, take a look at our tech-rider (<a href="https://drive.google.com/file/d/0B0RqX5ckmV-mTm4ySHRvOGJrVlU/">stereo</a> | <a href="https://drive.google.com/file/d/0B0RqX5ckmV-mbDhyRER0b2NPRW8/">surround</a>) and <a href="/contact/">contact us</a>.
+        <p><a href="http://sympliromatiko.net/video/">More videos</a>
+        """
+        # XXX instead of "More videos" add small pictograms with links to youtube & vimeo
+        },
+    "media": {
+        "title": "",
+        "text": """<iframe src="https://player.vimeo.com/video/134404722?title=0&byline=0&portrait=0" width="300" height="169" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>"""
+    }
+}
+
+def get_about():
+    return {
+    "main": {
+        "title": "MOON2",
+        "text": """<i>Denis Kolokol, electronics, movement;
+        <br>Ernest Ogórek, bass;
+        <br>Tomek Chołoniewski, drums.</i>
+        <p>
+        MOON2 is a trio with more or less stable residence in Kraków, Poland. The group’s main focus is interactive performance, algorithmic composition, virtual worlds, this kind of stuff... Pretty scientific and inhuman, but made by humans and for a humankind.
+        """
+        # XXX add small pictograms with links to full-size pictures
+        },
+    "aux": {
+        "title": "",
+        "text": """We see music not as a sequence of events, but as a dynamic system, which we only pretend to control. If an order becomes slightly more complicated, than the simple “if-else” directive, the system tends to fall apart as more and more hidden inconsistencies unfold. These anomalies is our material. We balance on the surface of the meticulously painted chart trying not to be buried under the pure chaos.
+        <p>
+        All the electronic sounds are programmed manually in <a href="http://supercollider.github.io/">SuperCollider</a>. No commercial synths used.
+        """
+        },
+    "media": {
+        "title": "",
+        "text": """<a href="/static/img/portrait.jpg"><img src="/static/img/portrait_s.jpg" border="0" /></a>"""
     }
 }
