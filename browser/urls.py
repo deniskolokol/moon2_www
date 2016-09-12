498f56ee6c90c678b23f18dc6a24ad65552a7e31
Denis Kolokol <dkolokol@gmail.com>
2016-09-12 13:44:06 +0200

    use models instead of pre-defined text

--- browser/urls.py
+++ browser/urls.py
@@ -4,36 +4,10 @@ from . import views
 
 
 urlpatterns = (
-    url(r'^$', views.view_index, {
-        # XXX - move this all to db
-        'label': 'index',
-        'template': 'column_left',
-        'img': 'bg1.jpg'
-        # XXX
-        }),
-    url(r'^watch/$', views.view_watch, {
-        'label': 'watch',
-        'template': 'row_top',
-        'img': 'bg5.jpg'
-        }),
-    url(r'^listen/$', views.view_listen, {
-        'label': 'listen',
-        'template': 'row_top',
-        'img': 'bg6.jpg'
-        }),
-    url(r'^read/$', views.view_read, {
-        'label': 'about',
-        'template': 'row_bottom',
-        'img': 'bg4.jpg'
-        }),
-    url(r'^rider/$', views.view_rider, {
-        'label': 'tech-rider',
-        'template': 'column_left',
-        'img': 'bg2.jpg'
-        }),
-    url(r'^contact/$', views.view_contact, {
-        'label': 'contact',
-        'template': 'row_top',
-        'img': 'bg3.jpg'
-        }),
+    url(r'^$', views.view_content, {'template': 'column_left', 'section': 'index'}),
+    url(r'^watch/$', views.view_content, {'template': 'row_top', 'section': 'watch'}),
+    url(r'^listen/$', views.view_content, {'template': 'row_top', 'section': 'listen'}),
+    url(r'^read/$', views.view_content, {'template': 'row_bottom', 'section': 'read'}),
+    url(r'^rider/$', views.view_content, {'template': 'column_left', 'section': 'rider'}),
+    url(r'^contact/$', views.view_content, {'template': 'row_top', 'section': 'contact'}),
 )
--- config/prod.py
+++ config/prod.py
@@ -1,1 +1,18 @@
 from config.common import *
+
+INSTALLED_APPS += ('storages',)
+AWS_STORAGE_BUCKET_NAME = "moon2-static"
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = S3_URL
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'moon2',
+        'USER' : 'moon2root',
+        'PASSWORD' : 'moon2',
+        'HOST' : 'dekoinst.cqwlunjmbkvr.eu-central-1.rds.amazonaws.com',
+        'PORT' : '5432',
+    }
+}
