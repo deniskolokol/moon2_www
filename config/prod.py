4166f0dc01955874d4a6bc9bc3de8059895630e1
Denis Kolokol <dkolokol@gmail.com>
2016-09-16 11:43:54 +0200

    revert to use local static files dir

--- config/prod.py
+++ config/prod.py
@@ -1,10 +1,12 @@
 from config.common import *
 
-INSTALLED_APPS += ('storages',)
-AWS_STORAGE_BUCKET_NAME = "moon2-static"
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-STATIC_URL = S3_URL
+# XXX - add this when moving to fabric
+#
+# INSTALLED_APPS += ('storages',)
+# AWS_STORAGE_BUCKET_NAME = "moon2-static"
+# STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+# S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+# STATIC_URL = S3_URL
 
 DATABASES = {
     'default': {
