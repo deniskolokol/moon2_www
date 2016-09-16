63c15e93cb712ca5a4f4d6ec824ce1c3ec926f06
Denis Kolokol <dkolokol@gmail.com>
2016-09-16 12:56:52 +0200

    correct path to urls

--- urls.py
+++ urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^', include('browser.urls')),
+    url(r'^', include('moon2_www.browser.urls')),
     url(r'^admin/', admin.site.urls),
 ]
