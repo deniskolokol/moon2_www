f47d5f5ee98723cc6f838dd17cb681b8b018de29
Denis Kolokol <dkolokol@gmail.com>
2016-12-27 14:35:29 +0100

    prod config overrides

--- config/prod.py
+++ config/prod.py
@@ -1,6 +1,8 @@
-DEBUG = False
-
 try:
     from config.local import *
 except ImportError:
     print 'Local settings not found, running on common.'
+
+# config.local imports config.base and adds necessary settings.
+# Overrides follow:
+DEBUG = False
