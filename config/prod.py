04a49d17c4fe8125712b5202208cbb56c0f12ed1
Denis Kolokol <dkolokol@gmail.com>
2017-11-30 11:47:41 +0100

    remove stale prod config file

--- config/prod.py
+++
@@ -1,8 +0,0 @@
-try:
-    from config.local import *
-except ImportError:
-    print 'Local settings not found, running on common.'
-
-# config.local imports config.base and adds necessary settings.
-# Overrides follow:
-DEBUG = False
